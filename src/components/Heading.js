1bc0d13164de3176c5fdb262290ab602010d0cad
Esteban Romo <esteban.romo.98@gmail.com>
2021-01-31 22:09:02 -0500

    Added Header component

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Heading.css'
+import DateFormat from 'dateformat'
+
+export default function Heading() {
+    return (
+        <div className='Heading'>
+            <h1>Tasks</h1>
+            <h3>{DateFormat(Date.now(),"dddd, mmmm dS")}</h3>
+        </div>
+    )
+}
