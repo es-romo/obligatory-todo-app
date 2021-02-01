f1017d62e4f02f1fe7181b2d270ae7dbf6a15550
Esteban Romo <esteban.romo.98@gmail.com>
2021-01-31 22:10:13 -0500

    Created Task and TaskList components

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Font, { Text } from 'react-font'
+
+export default class Task extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './TaskList.css'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <div className='TaskList'>
+                
+            </div>
+        )
+    }
+}
