f95a69b3f24ab99def75f217247352e8f16481b5
Esteban Romo <esteban.romo.98@gmail.com>
2021-02-01 23:02:20 -0500

    Modified TaskList and added CSS

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import '../styles/Task.css'
 
+import {FaPlus} from 'react-icons/fa'
+
 export default class Task extends Component {
     render() {
+        let icon = <FaPlus />
         return (
-            <div>
-                
+            <div className="Task">
+                <div className='Task-Container'>
+                    {icon}
+                    <p className="Task-Text">{this.props.task.text }</p>
+                </div>
             </div>
         )
     }
