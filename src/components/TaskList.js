4f97b8b312cdcf26ed573a1e12246063070d025f
Esteban Romo <esteban.romo.98@gmail.com>
2021-02-01 23:04:34 -0500

    Populated TaskList with dummy data

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import '../styles/TaskList.css'
 
+import Task from './Task'
+
 export default class TaskList extends Component {
+    constructor(){
+        super()
+        this.state= {
+            tasks: [
+                {
+                    text: 'Walk the dog',
+                    completed: false
+                },
+                {
+                    text: 'Eat Ass',
+                    completed: true
+                },
+                {
+                    text: 'Buy groceries',
+                    completed: false
+                }
+            ]
+        }
+    }
     render() {
+        let tasks = this.state.tasks.map( (task) => <Task task={task}/> )
+
         return (
             <div className='TaskList'>
-                
+                {tasks}
             </div>
         )
     }
